b57c9b62d9cf109134e09a86bf054a95134744d4
Eugen Morozov <jinseinoido@gmail.com>
2019-09-14 07:42:21 +0200

    add configs
    -- webpack
    --npm init
    -- babel

---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+//copy files from /src to /dist
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+
+module.exports = {
+  entry:  [ 'babel-polyfill',
+            './src/scripts/index.js',
+            './src/scss/style.scss'
+          ],
+          output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+          },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [ {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }],
+      },
+      {
+        test: /\.md$/i,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              camelCase: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }      
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack 4 Starter',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+          removeComments: true,
+          collapseWhitespace: false
+      }
+  }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].css'
+    }),
+    new CopyWebpackPlugin([
+    {
+      from: './content/images',
+      to: './images'
+    },
+    {
+      from: './content/thumbnails',
+      to: './thumbnails'
+    }
+    ])
+  ]
+};
