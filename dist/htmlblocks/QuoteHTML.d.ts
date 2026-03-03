a3a46c1c0046c75e144129e9c0acb9d38dc036c0
Eugen Morozov <meugenom@gmail.com>
2026-03-03 11:03:51 +0100

    FIXED version 1.1.0

--- dist/htmlblocks/QuoteHTML.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Token from "../Token";
-import "../static/styles/quote.css";
-export declare class QuoteHTML {
-    private DomUtilites;
-    private token;
-    private htmlOutput;
-    constructor(token: Token.quoteToken, htmlOutput: HTMLElement);
-    render(): void;
-}
