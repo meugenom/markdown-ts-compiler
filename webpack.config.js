a0c46ccedcded55c412f2ef693d15b11ddee82af
Eugen Morozov <26843480+meugenom@users.noreply.github.com>
2026-04-04 17:27:12 +0200

    webpack rules
    - add all fonts to sub-folder
    - make 1 chunk

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   mode: 'development',
   devtool: 'inline-source-map',
@@ -32,35 +34,50 @@ module.exports = {
             configFile: path.resolve(__dirname, './tsconfig.json'),
           },
         },
-      },
+      },      
       {
 		test:/\.css$/,
 		use:[
 		MiniCssExtractPlugin.loader,
-		"css-loader", "postcss-loader"]}
+		"css-loader", "postcss-loader"]
+    },
+    {
+      // find folnts
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+      // [hash] put to sub-folder
+      filename: 'assets/fonts/[name].[hash][ext][query]'
+        },
+      },
     ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   plugins: [
-	  new HtmlWebpackPlugin({
-		template: "./src/static/index.html",
+	
+    new HtmlWebpackPlugin({
+		  template: "./src/static/index.html",
     	filename: "./index.html"
 	  }),
 	  
 	  new MiniCssExtractPlugin({
-        filename:"style.css",
-		chunkFilename: "style.css"
+      filename:"style.css",
+		  chunkFilename: "style.css"
     }),
 	
-  new CopyPlugin({
-		patterns: [
-		  { from: "./src/content/images", to: "images" },
-		  { from: "./src/content/thumbnails", to: "thumbnails" },
-		],
+    new CopyPlugin({
+		  patterns: [
+		    { from: "./src/content/images", to: "images" },
+		    { from: "./src/content/thumbnails", to: "thumbnails" },
+		  ],
 	  }),
-    
+
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1, // One bundle file
+    }),
+
 ],
   devServer: {    
     static: {
